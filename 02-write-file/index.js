dfa1ce515ffa5b994104588b773c812fac9ca2af
Alexandra Samoilova <avsamoilava@gmail.com>
2022-11-02 20:52:34 +0300

    feat: add '02-write-file' solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const process = require('process');
+const path = require('path');
+const fs = require('fs');
+
+const outputStream = fs.createWriteStream(path.join(__dirname, 'output.txt'));
+
+process.stdout.write('Напишите что-нибудь:\n');
+
+process.stdin.on('data', data => {
+  if(data.toString().slice(0,-2) === 'exit') {
+    process.exit();
+  } else {
+    outputStream.write(data);
+  }
+})
+
+process.on('SIGINT', ()=>{
+  process.exit();
+});
+
+process.on('exit', ()=>process.stdout.write('Пока-пока!'));
