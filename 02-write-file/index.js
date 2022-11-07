d35cad75bd7cc6758976d8fe820a7172c76c8044
Alexandra Samoilova <avsamoilava@gmail.com>
2022-11-07 11:18:56 +0300

    refactor: correct prompt for input task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const outputStream = fs.createWriteStream(path.join(__dirname, 'output.txt'));
 
-process.stdout.write('Напишите что-нибудь:\n');
+process.stdout.write('Напишите что-нибудь и введите "enter":\n');
 
 process.stdin.on('data', data => {
   if(data.toString().slice(0,-2) === 'exit') {
